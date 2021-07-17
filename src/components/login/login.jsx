77f87c15f7b2083e6c0e0266f2e33bd86f29ba35
haejii <yanghagi@naver.com>
2021-07-18 01:20:43 +0900

    Implement Login Component

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Footer from '../footer/footer';
 import Header from '../header/header';
+import styles from './login.module.css';
+
 
 const Login = ({authService}) => {
 
@@ -10,13 +12,13 @@ const Login = ({authService}) => {
         .then(console.log);
     }
     return(
-        <section>
+        <section className={styles.Login}>
             <Header/>
             <section>
-                <h1>Login</h1>
-                <ul>
-                    <li><button onClick={onLogin}>Google</button></li>
-                    <li><button onClick={onLogin}>Github</button></li>
+                <h1 className={styles.title}>Login</h1>
+                <ul className={styles.list}>
+                    <li className={styles.lilist}><button className={styles.loginBtn} onClick={onLogin}>Google</button></li>
+                    <li className={styles.lilist}><button className={styles.loginBtn} onClick={onLogin}>Github</button></li>
                 </ul>
             </section>
             <Footer/>
