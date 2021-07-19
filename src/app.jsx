d9bf60ea0ee4f590616a01d8ccc8d54fd40b63df
haejii <yanghagi@naver.com>
2021-07-19 22:54:41 +0900

    Link route

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
 import Login from './components/login/login';
 import styles from './app.module.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  BrowserRouter
+} from "react-router-dom";
+import Maker from './components/maker/maker';
 
 function App({authService}) {
   return( 
     <div className={styles.app}>
-      <Login authService={authService}/>
+      <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+        <Login authService={authService}/>
+        </Route>
+        <Route exact path="/maker">
+        <Maker />
+        </Route>
+      </Switch>
+      </BrowserRouter>
+        
     </div>
   )
 }
